3fa2e5e30615bc32fc63453a87767d020363e2ef
Aileen McDonald <aileen.mcdonald@shopify.com>
2016-07-08 08:36:15 -0400

    Finish user signup

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160706195725) do
 
-  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
-    t.string   "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
